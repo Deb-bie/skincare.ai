8ec9b748f6e6dec185cc121ff6b5ef51eb1292d3
Deb-bie <asamoahjaeldeborah@gmail.com>
2025-10-19 20:59:25 -0500

    added routines to user model

--- lib/models/user/user_model.dart
+++ lib/models/user/user_model.dart
@@ -1,24 +1,42 @@
+import '../routine/routine_model.dart';
+
 class UserModel {
   final String id;
   final String username;
   final String email;
   final String password;
+  final Map<String, RoutineModel> routines;
 
   UserModel({
     required this.id,
     required this.username,
     required this.email,
-    required this.password
-  });
+    required this.password,
+    Map<String, RoutineModel>? routines,
+  })
+      : routines = routines ?? {};
+
+
 
 
 //   factory method to receive json data
   factory UserModel.fromJson(Map<String, dynamic> json) {
+    final routinesData = json['routines'] as Map<String, dynamic>?;
+
+    final routines = routinesData?.map(
+            (key, value) => MapEntry(
+                key, RoutineModel.fromJson(
+                Map<String, dynamic>.from(value)
+            )
+            )
+    ) ?? {};
+
     return UserModel(
         id: json['id'] ?? '',
         username: json['username'] ?? '',
         email: json['email'] ?? '',
-        password: json['password'] ?? ''
+        password: json['password'] ?? '',
+        routines: routines
     );
   }
 
@@ -29,7 +47,8 @@ class UserModel {
       'id': id,
       'username': username,
       'email': email,
-      'password': password
+      'password': password,
+      'routines': routines.map((key, value) => MapEntry(key, value.toJson())),
     };
   }
 
@@ -44,9 +63,8 @@ class UserModel {
         id: id,
         username: username ?? this.username,
         email: email ?? this.email,
-        password: password ?? this.password
+        password: password ?? this.password,
+        routines: {}
     );
   }
-
-
 }
