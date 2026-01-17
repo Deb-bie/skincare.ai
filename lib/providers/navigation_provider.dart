15d49b7ed9e1bf5990bfc1daec148bb2d18cdb11
Deb-bie <asamoahjaeldeborah@gmail.com>
2026-01-16 23:53:41 -0600

    added navigation provider

--- lib/providers/navigation_provider.dart
+++ lib/providers/navigation_provider.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/foundation.dart';
+import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
 
 class NavigationProvider extends ChangeNotifier {
-  int _selectedIndex = 0;
-  bool _isInSettingsPage = false;
+  final PersistentTabController _controller = PersistentTabController(initialIndex: 0);
 
-  int get selectedIndex => _selectedIndex;
+  bool _isInSettingsPage = false;
   bool get isInSettingsPage => _isInSettingsPage;
 
   void setIndex(int index) {
-    _selectedIndex = index;
+    _controller.jumpToTab(index);
     _isInSettingsPage = false;
     notifyListeners();
   }
