dda2028406379b27ec9669e249d8f63a86eacc5a
Deb-bie <asamoahjaeldeborah@gmail.com>
2026-01-25 23:28:24 -0600

    added logger

---
+++ lib/core/utils/app_logger.dart
@@ -0,0 +1,39 @@
+import 'dart:developer' as developer;
+import 'package:flutter/foundation.dart';
+
+class AppLogger {
+  static void log(String message, {String? name}) {
+    if (kDebugMode) {
+      developer.log(message, name: name ?? 'App');
+    }
+  }
+
+  static void debug(String message) {
+    if (kDebugMode) {
+      developer.log(message, name: 'DEBUG');
+    }
+  }
+
+  static void info(String message) {
+    if (kDebugMode) {
+      developer.log(message, name: 'INFO');
+    }
+  }
+
+  static void warning(String message) {
+    if (kDebugMode) {
+      developer.log(message, name: 'WARNING');
+    }
+  }
+
+  static void error(String message, [Object? error, StackTrace? stackTrace]) {
+    if (kDebugMode) {
+      developer.log(
+        message,
+        name: 'ERROR',
+        error: error,
+        stackTrace: stackTrace,
+      );
+    }
+  }
+}
